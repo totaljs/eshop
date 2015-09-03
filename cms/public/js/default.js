93e5ad9213def09df6a40d37a8f7934b61d5154d
Peter Širka <petersirka@gmail.com>
2015-09-03 23:50:35 +0200

    Add support for galleries (CMS).

--- cms/public/js/default.js
+++ cms/public/js/default.js
@@ -15,6 +15,105 @@ COMPONENT('emaildecode', function() {
 	};
 });
 
+COMPONENT('gallery', function() {
+
+	var self = this;
+	var layer;
+	var visible = false;
+
+	self.index = 0;
+	self.max = 0;
+
+	// For all galleries
+	if (!window.$gallery_init) {
+		$(document.body).append('<div id="gallery-layer"><a herf="javascript:void(0)" class="gallery-close"><span class="fa fa-times"></span></a><div class="gallery-container"><div class="gallery-prev"><span class="fa fa-arrow-left"></span></div><div class="gallery-image"><img src="/img/empty.png" class="img-responsive" /><div class="gallery-alt"></div></div><div class="gallery-next"><span class="fa fa-arrow-right"></span></div></div></div>');
+		window.$gallery_init = true;
+
+		$(window).on('keydown', function(e) {
+			if (!window.$gallery)
+				return;
+			if (e.keyCode === 39)
+				window.$gallery.next();
+			else if (e.keyCode === 37)
+				window.$gallery.prev();
+			else if (e.keyCode === 27)
+				window.$gallery.hide();
+		});
+
+		$(document).on('click', '.gallery-prev,.gallery-next,.gallery-close', function() {
+			var el = $(this);
+
+			if (el.hasClass('gallery-close')) {
+				window.$gallery.hide();
+				return;
+			}
+
+			if (el.hasClass('gallery-prev')) {
+				window.$gallery.prev();
+				return;
+			}
+
+			window.$gallery.next();
+		});
+	}
+
+	self.next = function() {
+		self.go(self.index + 1);
+	};
+
+	self.prev = function() {
+		self.go(self.index - 1);
+	};
+
+	self.hide = function() {
+		visible = false;
+		layer.hide();
+		window.$gallery = null;
+	};
+
+	self.go = function(index) {
+		if (index >= self.max)
+			self.index = 0;
+		else if (index < 0)
+			self.index = self.max - 1;
+		else
+			self.index = index;
+		self.show();
+	};
+
+	self.refresh = function() {
+		self.max = 0;
+		self.find('.gallery').each(function(index) {
+			var item = $(this);
+			var image = item.find('img');
+			item.append('<div class="gallery-info"><div>' + image.attr('alt') + '</div><span class="fa fa-camera"></span><b>' + image.attr('data-width') + 'x' + image.attr('data-height') + '</b></div>');
+			item.attr('data-index', index);
+			self.max++;
+		});
+	};
+
+	self.make = function() {
+		layer = $('#gallery-layer');
+		self.refresh();
+		self.element.on('click', '.gallery', function() {
+			var item = $(this);
+			index = parseInt(item.attr('data-index'));
+			self.show();
+		});
+	};
+
+	self.show = function() {
+		var img = self.find('.gallery[data-index="' + self.index + '"]').find('img');
+		layer.find('img').attr('src', img.attr('data-original'));
+		layer.find('.gallery-alt').html(img.attr('alt'));
+		if (visible)
+			return;
+		layer.show();
+		visible = true;
+		window.$gallery = self;
+	};
+});
+
 COMPONENT('newsletter', function() {
 	var self = this;
 	var button;
