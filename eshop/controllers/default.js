95fcfc84bc16a7aaa6d6aee9b5cb54349aec17de
Peter Širka <petersirka@gmail.com>
2015-09-03 22:43:49 +0200

    CMS supports better image editor (supports miniatures) for templating.

--- eshop/controllers/default.js
+++ eshop/controllers/default.js
@@ -49,31 +49,70 @@ function view_page() {
 // FILES
 // ==========================================================================
 
-// Reads a specific file from database
+// Reads specific file from database
+// For images (jpg, gif, png) supports percentual resize according "?s=NUMBER" argument in query string e.g.: .jpg?s=50, .jpg?s=80 (for image galleries)
 // URL: /download/*.*
 function file_read(req, res, is) {
 
 	if (is)
 		return req.path[0] === 'download';
 
-	// Reads specific file by ID
-	DB('files').binary.read(req.path[1].replace('.' + req.extension, ''), function(err, stream, header) {
-		if (err) {
-			res.throw404();
-			return;
-		}
-		res.stream(header.type, stream);
+	var id = req.path[1].replace('.' + req.extension, '');
+
+	if (!req.query.s || (req.extension !== 'jpg' && req.extension !== 'gif' && req.extension !== 'png')) {
+		// Reads specific file by ID
+		DB('files').binary.read(id, function(err, stream, header) {
+			if (err)
+				return res.throw404();
+			res.stream(header.type, stream);
+		});
+		return;
+	}
+
+	// Custom image resizing
+
+	// Small hack for the file cache.
+	// F.exists() uses req.uri.pathname for creating temp identificator and skips all query strings by creating (because this hack).
+	req.uri.pathname = req.uri.pathname.replace('.', req.query.s + '.');
+
+	// Below method checks if the file exists (processed) in temporary directory
+	// More information in total.js documentation
+	F.exists(req, res, 10, function(next, filename) {
+
+		// Reads specific file by ID
+		DB('files').binary.read(id, function(err, stream, header) {
+
+			if (err) {
+				next();
+				return res.throw404();
+			}
+
+			var writer = require('fs').createWriteStream(filename);
+			stream.pipe(writer);
+			stream.on('end', function() {
+
+				// Image processing
+				F.responseImage(req, res, filename, function(image) {
+					image.output(req.extension);
+					if (req.extension === 'jpg')
+						image.quality(85);
+					image.resize(req.query.s + '%');
+					image.minify();
+				});
+
+				// Releases F.exists()
+				next();
+			});
+		});
 	});
 }
 
-// Reads a specific picture from database
+// Reads specific picture from database
 // URL: /images/small|large/*.jpg
 function file_image(req, res, is) {
 
 	if (is)
 		return req.path[0] === 'images' && (req.path[1] === 'small' || req.path[1] === 'large') && req.path[2];
-
-	var Fs = require('fs');
 
 	// Below method checks if the file exists (processed) in temporary directory
 	// More information in total.js documentation
@@ -87,7 +126,7 @@ function file_image(req, res, is) {
 				return res.throw404();
 			}
 
-			var writer = Fs.createWriteStream(filename);
+			var writer = require('fs').createWriteStream(filename);
 			stream.pipe(writer);
 
 			stream.on('end', function() {
@@ -105,7 +144,7 @@ function file_image(req, res, is) {
 					image.minify();
 				});
 
-				// Release F.exists()
+				// Releases F.exists()
 				next();
 			});
 		});
