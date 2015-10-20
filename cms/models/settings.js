fc6ba17e3ff1a1b8c16da97dc936e2bfb3e042fb
Peter Širka <petersirka@gmail.com>
2015-10-20 12:49:58 +0200

    Improve schema's structure.

--- cms/models/settings.js
+++ cms/models/settings.js
@@ -1,72 +1,77 @@
 var Fs = require('fs');
 var filename = F.path.databases('settings.json');
 
-var SuperUser = NEWSCHEMA('SuperUser');
-SuperUser.define('login', String, true);
-SuperUser.define('password', String, true);
-SuperUser.define('roles', '[String]');
+NEWSCHEMA('SuperUser').make(function(schema) {
 
-var Settings = NEWSCHEMA('Settings');
-Settings.define('emailcontactform', String, true);
-Settings.define('emailreply', String, true);
-Settings.define('emailsender', String, true);
-Settings.define('url', String, true);
-Settings.define('templates', '[String]');
-Settings.define('navigations', '[String]');
-Settings.define('users', '[SuperUser]', true);
+	schema.define('login', String, true);
+	schema.define('password', String, true);
+	schema.define('roles', '[String]');
 
-// Saves settings into the file
-Settings.setSave(function(error, model, options, callback) {
-	var settings = U.extend({}, model.$clean());
-
-	if (settings.url.endsWith('/'))
-		settings.url = settings.url.substring(0, settings.url.length - 1);
-
-	// Writes settings into the file
-	Fs.writeFile(filename, JSON.stringify(settings), NOOP);
-
-	// Returns response
-	callback(SUCCESS(true));
 });
 
-// Gets settings
-Settings.setGet(function(error, model, options, callback) {
-	Fs.readFile(filename, function(err, data) {
-		var settings = {};
-		if (!err)
-			settings = JSON.parse(data.toString('utf8'));
-		callback(settings);
-	});
-});
+NEWSCHEMA('Settings').make(function(schema) {
 
-// Loads settings + rewrites framework configuration
-Settings.addWorkflow('load', function(error, model, options, callback) {
-	Settings.get(null, function(err, settings) {
+	schema.define('emailcontactform', String, true);
+	schema.define('emailreply', String, true);
+	schema.define('emailsender', String, true);
+	schema.define('url', String, true);
+	schema.define('templates', '[String]');
+	schema.define('navigations', '[String]');
+	schema.define('users', '[SuperUser]', true);
 
-		F.config.custom = settings;
+	// Saves settings into the file
+	schema.setSave(function(error, model, options, callback) {
+		var settings = U.extend({}, model.$clean());
 
-		// Refreshes internal informations
-		if (!F.config.custom.users)
-			F.config.custom.users = [];
+		if (settings.url.endsWith('/'))
+			settings.url = settings.url.substring(0, settings.url.length - 1);
 
-		// Adds an admin (service) account
-		var sa = CONFIG('manager-superadmin').split(':');
-		F.config.custom.users.push({ login: sa[0], password: sa[1], roles: [], sa: true });
-
-		// Optimized for the performance
-		var users = {};
-		for (var i = 0, length = F.config.custom.users.length; i < length; i++) {
-			var user = F.config.custom.users[i];
-			users[user.login + ':' + user.password] = user;
-		}
-
-		F.config.custom.users = users;
-
-		// Rewrites internal framework settings
-		F.config['mail.address.from'] = F.config.custom.emailsender;
-		F.config['mail.address.reply'] = F.config.custom.emailreply;
+		// Writes settings into the file
+		Fs.writeFile(filename, JSON.stringify(settings), NOOP);
 
 		// Returns response
 		callback(SUCCESS(true));
+	});
+
+	// Gets settings
+	schema.setGet(function(error, model, options, callback) {
+		Fs.readFile(filename, function(err, data) {
+			var settings = {};
+			if (!err)
+				settings = JSON.parse(data.toString('utf8'));
+			callback(settings);
+		});
+	});
+
+	// Loads settings + rewrites framework configuration
+	schema.addWorkflow('load', function(error, model, options, callback) {
+		schema.get(null, function(err, settings) {
+
+			F.config.custom = settings;
+
+			// Refreshes internal informations
+			if (!F.config.custom.users)
+				F.config.custom.users = [];
+
+			// Adds an admin (service) account
+			var sa = CONFIG('manager-superadmin').split(':');
+			F.config.custom.users.push({ login: sa[0], password: sa[1], roles: [], sa: true });
+
+			// Optimized for the performance
+			var users = {};
+			for (var i = 0, length = F.config.custom.users.length; i < length; i++) {
+				var user = F.config.custom.users[i];
+				users[user.login + ':' + user.password] = user;
+			}
+
+			F.config.custom.users = users;
+
+			// Rewrites internal framework settings
+			F.config['mail.address.from'] = F.config.custom.emailsender;
+			F.config['mail.address.reply'] = F.config.custom.emailreply;
+
+			// Returns response
+			callback(SUCCESS(true));
+		});
 	});
 });
