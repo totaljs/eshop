4b914264ceb4f1f0d35bca4d68f5f1e8915ca2fe
Peter Širka <petersirka@gmail.com>
2015-12-16 08:42:08 +0100

    Updated OAuth2 module (many improvements).

--- eshop-mongodb/modules/oauth2.js
+++ eshop-mongodb/modules/oauth2.js
@@ -4,35 +4,29 @@
 var Qs = require('querystring');
 
 exports.id = 'oauth2';
-exports.version = '1.20';
+exports.version = '1.3';
 
-var stats = { facebook: 0, google: 0,linkedin: 0, yahoo: 0, dropbox: 0, github: 0, yandex: 0, instagram: 0, twitter: 0, vk: 0 };
+var stats = { facebook: 0, google: 0,linkedin: 0, yahoo: 0, dropbox: 0, github: 0, yandex: 0, instagram: 0, vk: 0, live: 0 };
+var OAUTH2_HEADER = { code: '', client_id: '', client_secret: '', redirect: '', grant_type: 'authorization_code' };
+var OAUTH2_BEARER = { Authorization: '', 'User-Agent': 'total.js' };
+var FLAG_POST = ['post'];
+var FLAG_GET = ['get'];
 
 exports.usage = function() {
 	return stats;
 };
 
 function facebook_redirect(key, url) {
-	return 'https://graph.facebook.com/oauth/authorize?type=web_server&client_id={0}&redirect_uri={1}&scope=email,user_birthday,user_hometown'.format(key, encodeURIComponent(url));
+	return 'https://graph.facebook.com/oauth/authorize?type=web_server&client_id={0}&redirect_uri={1}&scope=email'.format(key, encodeURIComponent(url));
 }
 
 function facebook_profile(key, secret, code, url, callback) {
-	U.request('https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}'.format(key, url, secret, code), ['get'], '', function(err, data, status, headers) {
-
+	U.request('https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&client_secret={2}&code={3}'.format(key, url, secret, code), FLAG_GET, '', function(err, data) {
 		if (err)
-			return callback(err, null);
-
+			return callback(err);
 		if (data.indexOf('"error"') !== -1)
-			return callback(JSON.parse(data), null);
-
-		U.request('https://graph.facebook.com/me?' + data + '&fields=email,first_name,last_name,gender,hometown,locale,name,id,timezone,picture', ['get'], '', function(err, data, status) {
-
-			if (err)
-				return callback(err, null);
-
-			stats.facebook++;
-			callback(null, JSON.parse(data));
-		});
+			return callback(data);
+		U.request('https://graph.facebook.com/me?' + data + '&fields=email,first_name,last_name,gender,hometown,locale,name,id,timezone,picture', FLAG_GET, '', process('facebook', callback));
 	});
 }
 
@@ -41,22 +35,17 @@ function google_redirect(key, url) {
 }
 
 function google_profile(key, secret, code, url, callback) {
-	U.request('https://www.googleapis.com/oauth2/v3/token', ['post'], { code: code, client_id: key, client_secret: secret, redirect_uri: url, grant_type: 'authorization_code' }, function(err, data, status, headers) {
 
-		if (err)
-			return callback(err, null);
+	OAUTH2_HEADER.code = code;
+	OAUTH2_HEADER.client_id = key;
+	OAUTH2_HEADER.client_secret = secret;
+	OAUTH2_HEADER.redirect_uri = url;
 
-		if (data.indexOf('"error"') !== -1)
-			return callback(JSON.parse(data), null);
-
-		data = JSON.parse(data);
-		U.request('https://www.googleapis.com/plus/v1/people/me', ['get'], '', function(err, data, status) {
-			if (err)
-				return callback(err, null);
-			var user = JSON.parse(data);
-			stats.google++;
-			callback(null, user);
-		}, null, { 'Authorization': 'Bearer ' + data.access_token });
+	U.request('https://www.googleapis.com/oauth2/v3/token', FLAG_POST, OAUTH2_HEADER, function(err, data) {
+		if (!process_error(err, data, callback))
+			return;
+		OAUTH2_BEARER.Authorization = 'Bearer ' + data.parseJSON().access_token;
+		U.request('https://www.googleapis.com/plus/v1/people/me', FLAG_GET, '', process('google', callback), null, OAUTH2_BEARER);
 	});
 }
 
@@ -65,22 +54,17 @@ function linkedin_redirect(key, url) {
 }
 
 function linkedin_profile(key, secret, code, url, callback) {
-	U.request('https://www.linkedin.com/uas/oauth2/accessToken', ['post'], { code: code, client_id: key, client_secret: secret, redirect_uri: url, grant_type: 'authorization_code' }, function(err, data, status, headers) {
 
-		if (err)
-			return callback(err, null);
+	OAUTH2_HEADER.code = code;
+	OAUTH2_HEADER.client_id = key;
+	OAUTH2_HEADER.client_secret = secret;
+	OAUTH2_HEADER.redirect_uri = url;
 
-		if (data.indexOf('"error"') !== -1)
-			return callback(JSON.parse(data), null);
-
-		data = JSON.parse(data);
-		U.request('https://api.linkedin.com/v1/people/~:(id,first-name,last-name,headline,member-url-resources,picture-url,location,public-profile-url,email-address)?format=json', ['get'], '', function(err, data, status) {
-			if (err)
-				return callback(err, null);
-			var user = JSON.parse(data);
-			stats.linkedin++;
-			callback(null, user);
-		}, null, { 'Authorization': 'Bearer ' + data.access_token });
+	U.request('https://www.linkedin.com/uas/oauth2/accessToken', FLAG_POST, OAUTH2_HEADER, function(err, data) {
+		if (!process_error(err, data, callback))
+			return;
+		OAUTH2_BEARER.Authorization = 'Bearer ' + data.parseJSON().access_token;
+		U.request('https://api.linkedin.com/v1/people/~:(id,first-name,last-name,headline,member-url-resources,picture-url,location,public-profile-url,email-address)?format=json', FLAG_GET, '', process('linkedin', callback), null, OAUTH2_BEARER);
 	});
 }
 
@@ -89,42 +73,17 @@ function yahoo_redirect(key, url) {
 }
 
 function yahoo_profile(key, secret, code, url, callback) {
-	U.request('https://api.login.yahoo.com/oauth2/get_token', ['post'], { code: code, client_id: key, client_secret: secret, redirect_uri: url, grant_type: 'authorization_code' }, function(err, data, status, headers) {
 
-		if (err)
-			return callback(err, null);
+	OAUTH2_HEADER.code = code;
+	OAUTH2_HEADER.client_id = key;
+	OAUTH2_HEADER.client_secret = secret;
+	OAUTH2_HEADER.redirect_uri = url;
 
-		if (data.indexOf('"error"') !== -1)
-			return callback(JSON.parse(data), null);
-
-		data = JSON.parse(data);
-		U.request('https://social.yahooapis.com/v1/user/' + data.xoauth_yahoo_guid + '/profile?format=json', ['get'], '', function(err, data, status) {
-			if (err)
-				return callback(err, null);
-			var user = JSON.parse(data);
-			stats.yahoo++;
-			callback(null, user);
-		}, null, { 'Authorization': 'Bearer ' + data.access_token });
-	}, null, { 'Authorization': 'Basic ' + new Buffer(key + ':' + secret).toString('base64'), 'Content-Type': 'application/x-www-form-urlencoded' });
-}
-
-function twitter_profile(key, secret, code, url, callback) {
-	U.request('https://api.login.yahoo.com/oauth2/get_token', ['post'], { code: code, client_id: key, client_secret: secret, redirect_uri: url, grant_type: 'authorization_code' }, function(err, data, status, headers) {
-
-		if (err)
-			return callback(err, null);
-
-		if (data.indexOf('"error"') !== -1)
-			return callback(JSON.parse(data), null);
-
-		data = JSON.parse(data);
-		U.request('https://social.yahooapis.com/v1/user/' + data.xoauth_yahoo_guid + '/profile?format=json', ['get'], '', function(err, data, status) {
-			if (err)
-				return callback(err, null);
-			var user = JSON.parse(data);
-			stats.twitter++;
-			callback(null, user);
-		}, null, { 'Authorization': 'Bearer ' + data.access_token });
+	U.request('https://api.login.yahoo.com/oauth2/get_token', FLAG_POST, OAUTH2_HEADER, function(err, data) {
+		if (!process_error(err, data, callback))
+			return;
+		OAUTH2_BEARER.Authorization = 'Bearer ' + data.parseJSON().access_token;
+		U.request('https://social.yahooapis.com/v1/user/' + data.xoauth_yahoo_guid + '/profile?format=json', FLAG_GET, '', process('yahoo', callback), null, OAUTH2_BEARER);
 	}, null, { 'Authorization': 'Basic ' + new Buffer(key + ':' + secret).toString('base64'), 'Content-Type': 'application/x-www-form-urlencoded' });
 }
 
@@ -134,22 +93,16 @@ function github_redirect(key, url) {
 
 function github_profile(key, secret, code, url, callback) {
 
-	U.request('https://github.com/login/oauth/access_token', ['post'], { code: code, client_id: key, client_secret: secret, redirect_uri: url, grant_type: 'authorization_code' }, function(err, data, status, headers) {
+	OAUTH2_HEADER.code = code;
+	OAUTH2_HEADER.client_id = key;
+	OAUTH2_HEADER.client_secret = secret;
+	OAUTH2_HEADER.redirect_uri = url;
 
+	U.request('https://github.com/login/oauth/access_token', FLAG_POST, OAUTH2_HEADER, function(err, data) {
 		if (err)
-			return callback(err, null);
-
-		if (data.indexOf('"error"') !== -1)
-			return callback(Qs.parse(data), null);
-
-		data = Qs.parse(data);
-		U.request('https://api.github.com/user', ['get'], '', function(err, data, status) {
-			if (err)
-				return callback(err, null);
-			var user = JSON.parse(data);
-			stats.github++;
-			callback(null, user);
-		}, null, { 'Authorization': 'Bearer ' + data.access_token, 'User-Agent': 'total.js' });
+			return callback(err);
+		OAUTH2_BEARER.Authorization = 'Bearer ' + Qs.parse(data).access_token;
+		U.request('https://api.github.com/user', FLAG_GET, '', process('github', callback), null, OAUTH2_BEARER);
 	});
 }
 
@@ -158,22 +111,17 @@ function dropbox_redirect(key, url) {
 }
 
 function dropbox_profile(key, secret, code, url, callback) {
-	U.request('https://api.dropbox.com/1/oauth2/token', ['post'], { code: code, client_id: key, client_secret: secret, redirect_uri: url, grant_type: 'authorization_code' }, function(err, data, status, headers) {
 
-		if (err)
-			return callback(err, null);
+	OAUTH2_HEADER.code = code;
+	OAUTH2_HEADER.client_id = key;
+	OAUTH2_HEADER.client_secret = secret;
+	OAUTH2_HEADER.redirect_uri = url;
 
-		if (data.indexOf('"error"') !== -1)
-			return callback(JSON.parse(data), null);
-
-		data = JSON.parse(data);
-		U.request('https://api.dropbox.com/1/account/info', ['get'], '', function(err, data, status) {
-			if (err)
-				return callback(err, null);
-			var user = JSON.parse(data);
-			stats.dropbox++;
-			callback(null, user);
-		}, null, { 'Authorization': 'Bearer ' + data.access_token });
+	U.request('https://api.dropbox.com/1/oauth2/token', FLAG_POST, OAUTH2_HEADER, function(err, data) {
+		if (!process_error(err, data, callback))
+			return;
+		OAUTH2_BEARER.Authorization = 'Bearer ' + data.parseJSON().access_token;
+		U.request('https://api.dropbox.com/1/account/info', FLAG_GET, '', process('dropbox', callback), null, OAUTH2_BEARER);
 	});
 }
 
@@ -182,22 +130,17 @@ function live_redirect(key, url) {
 }
 
 function live_profile(key, secret, code, url, callback) {
-	U.request('https://login.live.com/oauth20_token.srf', ['post'], { code: code, client_id: key, client_secret: secret, redirect_uri: url, grant_type: 'authorization_code' }, function(err, data, status, headers) {
 
-		if (err)
-			return callback(err, null);
+	OAUTH2_HEADER.code = code;
+	OAUTH2_HEADER.client_id = key;
+	OAUTH2_HEADER.client_secret = secret;
+	OAUTH2_HEADER.redirect_uri = url;
 
-		if (data.indexOf('"error"') !== -1)
-			return callback(JSON.parse(data), null);
-
-		data = JSON.parse(data);
-		U.request('https://apis.live.net/v5.0/me/', ['get'], '', function(err, data, status) {
-			if (err)
-				return callback(err, null);
-			var user = JSON.parse(data);
-			stats.dropbox++;
-			callback(null, user);
-		}, null, { 'Authorization': 'Bearer ' + data.access_token });
+	U.request('https://login.live.com/oauth20_token.srf', FLAG_POST, OAUTH2_HEADER, function(err, data) {
+		if (!process_error(err, data, callback))
+			return;
+		OAUTH2_BEARER.Authorization = 'Bearer ' + data.parseJSON().access_token;
+		U.request('https://apis.live.net/v5.0/me/', FLAG_GET, '', process('live', callback), null, OAUTH2_BEARER);
 	});
 }
 
@@ -206,16 +149,16 @@ function instagram_redirect(key, url) {
 }
 
 function instagram_profile(key, secret, code, url, callback) {
-	U.request('https://api.instagram.com/oauth/access_token', ['post'], { code: code, client_id: key, client_secret: secret, redirect_uri: url, grant_type: 'authorization_code' }, function(err, data, status, headers) {
-		if (err)
-			return callback(err, null);
-		if (data.indexOf('"error"') !== -1)
-			return callback(JSON.parse(data), null);
-		var obj = JSON.parse(data);
-		if (!obj || !obj.user)
-			return callback(new Error(obj));
-		stats.instagram++;
-		callback(null, obj.user);
+
+	OAUTH2_HEADER.code = code;
+	OAUTH2_HEADER.client_id = key;
+	OAUTH2_HEADER.client_secret = secret;
+	OAUTH2_HEADER.redirect_uri = url;
+
+	U.request('https://api.instagram.com/oauth/access_token', FLAG_POST, OAUTH2_HEADER, function(err, data) {
+		if (!process_error(err, data, callback))
+			return;
+		process('instagram', callback)(null, data);
 	});
 }
 
@@ -224,22 +167,17 @@ function yandex_redirect(key, url) {
 }
 
 function yandex_profile(key, secret, code, url, callback) {
-	U.request('https://oauth.yandex.com/token', ['post'], { code: code, client_id: key, client_secret: secret, redirect_uri: url, grant_type: 'authorization_code' }, function(err, data, status, headers) {
 
-		if (err)
-			return callback(err, null);
+	OAUTH2_HEADER.code = code;
+	OAUTH2_HEADER.client_id = key;
+	OAUTH2_HEADER.client_secret = secret;
+	OAUTH2_HEADER.redirect_uri = url;
 
-		if (data.indexOf('"error"') !== -1)
-			return callback(JSON.parse(data), null);
-
-		data = JSON.parse(data);
-		U.request('https://login.yandex.ru/info', ['get'], '', function(err, data, status) {
-			if (err)
-				return callback(err, null);
-			var user = JSON.parse(data);
-			stats.yandex++;
-			callback(null, user);
-		}, null, { 'Authorization': 'Bearer ' + data.access_token });
+	U.request('https://oauth.yandex.com/token', FLAG_POST, OAUTH2_HEADER, function(err, data) {
+		if (!process_error(err, data, callback))
+			return;
+		OAUTH2_BEARER.Authorization = 'Bearer ' + data.parseJSON().access_token;
+		U.request('https://login.yandex.ru/info', FLAG_GET, '', process('yandex', callback), null, OAUTH2_BEARER);
 	});
 }
 
@@ -248,24 +186,69 @@ function vk_redirect(key, url) {
 }
 
 function vk_profile(key, secret, code, url, callback) {
-	U.request('https://oauth.vk.com/access_token', ['post'], { code: code, client_id: key, client_secret: secret, redirect_uri: url, grant_type: 'authorization_code' }, function(err, data, status, headers) {
 
-		if (err)
-			return callback(err, null);
+	OAUTH2_HEADER.code = code;
+	OAUTH2_HEADER.client_id = key;
+	OAUTH2_HEADER.client_secret = secret;
+	OAUTH2_HEADER.redirect_uri = url;
 
-		if (data.indexOf('"error"') !== -1)
-			return callback(JSON.parse(data), null);
-
-		data = JSON.parse(data);
-
-		U.request('https://api.vk.com/method/users.get', ['get'], 'uid=' + data.user_id + '&access_token=' + data.access_token + '&fields=nickname,screen_name,photo_big,sex,country,email', function(err, data, status) {
-			if (err)
-				return callback(err, null);
-			var user = JSON.parse(data);
-			stats.vk++;
-			callback(null, user);
-		}, null);
+	U.request('https://oauth.vk.com/access_token', FLAG_POST, OAUTH2_HEADER, function(err, data) {
+		if (!process_error(err, data, callback))
+			return;
+		data = data.parseJSON();
+		U.request('https://api.vk.com/method/users.get', FLAG_GET, 'uid=' + data.user_id + '&access_token=' + data.access_token + '&fields=nickname,screen_name,photo_big,sex,country,email', process('vk', callback));
 	});
+}
+
+function process_error(err, data, callback) {
+
+	if (err) {
+		callback(err);
+		return;
+	}
+
+	if (!data.trim().isJSON()) {
+		callback(data);
+		return;
+	}
+
+	if (data.indexOf('"error"') !== -1) {
+		callback(data.parseJSON());
+		return;
+	}
+
+	return true;
+}
+
+function process(name, callback) {
+	return function(err, data) {
+
+		stats[name]++;
+
+		if (err) {
+			callback(err);
+			return;
+		}
+
+		var user = data.parseJSON();
+
+		if (!user) {
+			callback(new Error('User data from OAuth2 ' + name + ' doesn\'t exist.'), user);
+			return;
+		}
+
+		if (name === 'yahoo') {
+			if (!user.profile && !user.guid) {
+				err = user;
+				user = null;
+			}
+		} else if (!user.id && !user.uid) {
+			err = user;
+			user = null;
+		}
+
+		callback(err, user);
+	};
 }
 
 exports.redirect = function(type, key, url, controller) {
