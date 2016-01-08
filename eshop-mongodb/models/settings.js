3953bee870ba3128040c0ab5cc3ef3b5d4f45327
Gera G. Güiles <gerardo.gonzalezg@gmail.com>
2016-01-09 00:09:35 +0100

    Update models/settings.js
    Added Brazilian currency
    http://assemblysys.com/iso-4217-currency-codes/

--- eshop-mongodb/models/settings.js
+++ eshop-mongodb/models/settings.js
@@ -68,6 +68,9 @@ NEWSCHEMA('Settings').make(function(schema) {
 			case 'czk':
 				settings.currency_entity = 'Kč';
 				break;
+			case 'brl':
+				settings.currency_entity = 'R$';
+				break;				
 			default:
 				settings.currency_entity = settings.currency;
 				break;
