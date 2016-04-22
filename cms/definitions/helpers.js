df26feaa4678497caab6da50f9b86c0faf571980
Peter Širka <petersirka@gmail.com>
2016-04-22 07:24:22 +0200

    Improved pagination.

--- cms/definitions/helpers.js
+++ cms/definitions/helpers.js
@@ -1,8 +1,5 @@
 // Helper for pagination rendering
 // Eshop uses this helper
 F.helpers.pagination = function(model) {
-	var builder = '';
-	var pagination = new Pagination(model.count, model.page, model.limit);
-	pagination.render().forEach(n => builder += '<a href="?page=' + n.page + '"' + (n.selected ? ' class="selected"' : '') + '>' + n.page + '</a>');
-	return builder;
+	return new Pagination(model.count, model.page, model.limit).html(8);
 };
--- eshop-mongodb/definitions/helpers.js
+++ eshop-mongodb/definitions/helpers.js
@@ -1,10 +1,7 @@
 // Helper for pagination rendering
 // Eshop uses this helper
 F.helpers.pagination = function(model) {
-	var builder = '';
-	var pagination = new Pagination(model.count, model.page, model.limit);
-	pagination.render().forEach(n => builder += '<a href="?page=' + n.page + '"' + (n.selected ? ' class="selected"' : '') + '>' + n.page + '</a>');
-	return builder;
+	return new Pagination(model.count, model.page, model.limit).html(8);
 };
 
 // Parses all parent categories from selected category
