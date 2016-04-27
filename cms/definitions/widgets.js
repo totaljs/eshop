6f6c88b966e8dcfdf098d94a01fddc3545006443
Peter Širka <petersirka@gmail.com>
2016-04-27 11:18:45 +0200

    Updated blog widget.

--- cms/definitions/widgets.js
+++ cms/definitions/widgets.js
@@ -5,34 +5,12 @@
 // Blogs
 // d5467a1697 is Widget ID
 GETSCHEMA('Widget').addTransform('d5467a1697', function(error, model, data, callback) {
-
 	// data.settings
 	// data.page
 	// data.controller
-
-	var prepare = function(doc) {
-		if (!doc.navigations)
-			return;
-		if (doc.navigations.indexOf('blogs') === -1)
-			return;
-		return { title: doc.title, url: doc.url, perex: doc.perex, datecreated: Date.parse(doc.datecreated), pictures: doc.pictures };
-	};
-
-	var sort = function(a, b) {
-		if (a.datecreated > b.datecreated)
-			return -1;
-		return 1;
-	};
-
-	DB('pages').sort(prepare, sort, function(err, docs) {
-		if (err)
-			return callback('');
-
-		var model = {};
-		model.max = U.parseInt(data.settings, 5);
-		model.items = docs;
-		callback(F.view('widgets/blogs', model));
-	});
+	var options = {};
+	options.max = U.parseInt(data.settings, 5);
+	GETSCHEMA('Post').query(options, (err, response) => callback(F.view('partial-blogs', response)));
 });
 
 // Contact form
