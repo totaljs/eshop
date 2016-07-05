2a350d1200e133692af7daf591f2929212e829f9
Peter Širka <petersirka@gmail.com>
2016-07-05 11:44:34 +0200

    Updated `click` jComponent.

--- eshop/public/js/ui.js
+++ eshop/public/js/ui.js
@@ -1,27 +1,34 @@
 COMPONENT('click', function() {
 	var self = this;
 
+	self.readonly();
+
+	self.click = function() {
+		var value = self.attr('data-value');
+		if (typeof(value) === 'string')
+			self.set(self.parser(value));
+		else
+			self.get(self.attr('data-component-path'))(self);
+	};
+
 	self.make = function() {
 
-		self.element.on('click', function() {
-			self.get(self.attr('data-component-path'))();
-		});
+		self.element.on('click', self.click);
 
 		var enter = self.attr('data-enter');
 		if (!enter)
 			return;
+
 		$(enter).on('keydown', 'input', function(e) {
 			if (e.keyCode !== 13)
 				return;
 			setTimeout(function() {
-				if (self.element.get(0).disabled === true)
+				if (self.element.get(0).disabled)
 					return;
-				self.get(self.attr('data-component-path'))();
+				self.click();
 			}, 100);
 		});
 	};
-
-	self.readonly();
 });
 
 COMPONENT('visible', function() {
