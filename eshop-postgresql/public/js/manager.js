4a836198bec35d5a5ffdf087f73c3aab44413ec3
Peter Širka <petersirka@gmail.com>
2016-08-26 08:23:13 +0200

    Improved code.

--- eshop-postgresql/public/js/manager.js
+++ eshop-postgresql/public/js/manager.js
@@ -14,10 +14,7 @@ $(document).ready(function() {
 		el.toggleClass('hidden', su.roles.length && su.roles.indexOf(el.attr('data-role')) === -1);
 	});
 
-	FIND('loading', function() {
-		this.hide(500);
-	});
-
+	FIND('loading', FN('() => this.hide(500)'));
 	$(window).on('resize', resizer);
 	resizer();
 });
@@ -117,13 +114,13 @@ function success() {
 }
 
 function can(name) {
-	if (su.roles.length === 0)
+	if (!su.roles.length)
 		return true;
 	return su.roles.indexOf(name) !== -1;
 }
 
 Tangular.register('price', function(value, format) {
-	if (value === undefined)
+	if (value == null)
 		value = 0;
 	return currency.format(value.format(format));
 });
@@ -135,26 +132,10 @@ Tangular.register('join', function(value) {
 });
 
 Tangular.register('default', function(value, def) {
-	if (value === undefined || value === null || value === '')
+	if (value == null || value === '')
 		return def;
 	return value;
 });
-
-jQuery.easing.easeOutBounce = function(e, f, a, h, g) {
-	if ((f /= g) < (1 / 2.75)) {
-		return h * (7.5625 * f * f) + a
-	} else {
-		if (f < (2 / 2.75)) {
-			return h * (7.5625 * (f -= (1.5 / 2.75)) * f + 0.75) + a
-		} else {
-			if (f < (2.5 / 2.75)) {
-				return h * (7.5625 * (f -= (2.25 / 2.75)) * f + 0.9375) + a
-			} else {
-				return h * (7.5625 * (f -= (2.625 / 2.75)) * f + 0.984375) + a
-			}
-		}
-	}
-};
 
 function getSelectionStartNode(context){
 	if (!context.getSelection)
