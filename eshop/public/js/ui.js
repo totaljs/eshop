2e4a547fb157019409e26fc3246dea6ee7898884
Peter Širka <petersirka@gmail.com>
2016-11-23 15:32:29 +0100

    Fix `disable` component.

--- eshop/public/js/ui.js
+++ eshop/public/js/ui.js
@@ -2056,48 +2056,6 @@ COMPONENT('tabmenu', function() {
 	};
 });
 
-COMPONENT('disable', function() {
-	var self = this;
-	var condition;
-	var selector;
-	var validate;
-
-	self.readonly();
-
-	self.make = function() {
-		condition = self.attr('data-if');
-		selector = self.attr('data-selector') || 'input,texarea,select';
-		validate = self.attr('data-validate');
-		if (validate)
-			validate = validate.split(',').trim();
-	};
-
-	self.setter = function(value) {
-		var is = true;
-
-		if (condition)
-			is = EVALUATE(self.path, condition);
-		else
-			is = value ? false : true;
-
-		self.find(selector).each(function() {
-			var el = $(this);
-			var tag = el.get(0).tagName;
-			if (tag === 'INPUT' || tag === 'SELECT') {
-				el.prop('disabled', is);
-				el.parent().toggleClass('ui-disabled', is);
-			} else
-				el.toggleClass('ui-disabled', is);
-		});
-
-		validate && validate.forEach(function(key) { jC.reset(key); });
-	};
-
-	self.state = function(type) {
-		self.update();
-	};
-});
-
 COMPONENT('confirm', function() {
 	var self = this;
 	var is = false;
@@ -2847,6 +2805,48 @@ COMPONENT('textboxlist', function() {
 // ==========================================================
 // @{end}
 // ==========================================================
+
+COMPONENT('disable', function() {
+	var self = this;
+	var condition;
+	var selector;
+	var validate;
+
+	self.readonly();
+
+	self.make = function() {
+		condition = self.attr('data-if');
+		selector = self.attr('data-selector') || 'input,texarea,select';
+		validate = self.attr('data-validate');
+		if (validate)
+			validate = validate.split(',').trim();
+	};
+
+	self.setter = function(value) {
+		var is = true;
+
+		if (condition)
+			is = EVALUATE(self.path, condition);
+		else
+			is = value ? false : true;
+
+		self.find(selector).each(function() {
+			var el = $(this);
+			var tag = el.get(0).tagName;
+			if (tag === 'INPUT' || tag === 'SELECT') {
+				el.prop('disabled', is);
+				el.parent().toggleClass('ui-disabled', is);
+			} else
+				el.toggleClass('ui-disabled', is);
+		});
+
+		validate && validate.forEach(function(key) { jC.reset(key); });
+	};
+
+	self.state = function(type) {
+		self.update();
+	};
+});
 
 COMPONENT('search', function() {
 
