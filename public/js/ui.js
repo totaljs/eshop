d85be9927eb94e0f8e18f00bca51c1fcb065dff3
Peter Širka <petersirka@gmail.com>
2016-12-04 00:14:56 +0100

    Update a cookie component.

--- public/js/ui.js
+++ public/js/ui.js
@@ -602,19 +602,49 @@ COMPONENT('error', function() {
 
 COMPONENT('cookie', function() {
 	var self = this;
-	self.readonly();
 	self.singleton();
+	self.readonly();
+
+	self.cancel = function() {
+		document.cookie.split(';').forEach(function(key) {
+			jC.cookies.set(key.split('=')[0], '', '-2 days');
+		});
+		try {
+			Object.keys(localStorage).forEach(function(key) {
+				localStorage.removeItem(key);
+			});
+		} catch (e) {}
+		location.href = 'about:blank';
+		return self;
+	};
+
 	self.make = function() {
-		var cookie = localStorage.getItem('cookie');
+
+		var cookie;
+
+		// private mode
+		try {
+			cookie = localStorage.getItem('cookie');
+		} catch (e) {}
+
 		if (cookie) {
 			self.element.addClass('hidden');
 			return;
 		}
 
 		self.element.removeClass('hidden').addClass('ui-cookie');
-		self.element.append('<button>' + (self.attr('data-button') || 'OK') + '</button>');
+		self.element.append('<button name="agree">' + (self.attr('data-agree') || 'OK') + '</button>');
+		self.element.append('<button name="cancel">' + (self.attr('data-cancel') || 'Cancel') + '</button>');
+
 		self.element.on('click', 'button', function() {
-			localStorage.setItem('cookie', '1');
+
+			if (this.name === 'cancel')
+				return self.cancel();
+
+			// private mode
+			try {
+				localStorage.setItem('cookie', '1');
+			} catch (e) {}
 			self.element.addClass('hidden');
 		});
 	};
